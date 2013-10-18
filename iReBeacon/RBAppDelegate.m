1ca6316d2ff6cd5781c4fc764d5dcdcb2b90f19b
Giuseppe Macrì <macri.giuseppe@gmail.com>
2013-10-18 00:01:45 -0700

    Imported facebook

--- iReBeacon/RBAppDelegate.m
+++ iReBeacon/RBAppDelegate.m
@@ -11,6 +11,9 @@
 // Parse Framework
 #import <Parse/Parse.h>
 
+// Facebook SDK
+#import <FacebookSDK/FacebookSDK.h>
+
 @implementation RBAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -30,6 +33,9 @@
     [defaultACL setPublicReadAccess:YES];
     [defaultACL setPublicWriteAccess:YES];
     [PFACL setDefaultACL:defaultACL withAccessForCurrentUser:YES];
+    
+    
+    
     
     return YES;
 }
@@ -59,6 +65,14 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+- (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+  sourceApplication:(NSString *)sourceApplication
+         annotation:(id)annotation
+{
+    return [FBSession.activeSession handleOpenURL:url];
 }
 
 @end
