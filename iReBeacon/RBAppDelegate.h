96fbf77eefe1b27d1b7c426b0d7c84d6e433b614
Giuseppe Macrì <macri.giuseppe@gmail.com>
2013-10-17 23:07:26 -0700

    Initial Commit

---
+++ iReBeacon/RBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RBAppDelegate.h
+//  iReBeacon
+//
+//  Created by Giuseppe Macrì on 10/17/13.
+//  Copyright (c) 2013 giuseppem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iReBeacon/RBViewController.h
@@ -0,0 +1,13 @@
+//
+//  RBViewController.h
+//  iReBeacon
+//
+//  Created by Giuseppe Macrì on 10/17/13.
+//  Copyright (c) 2013 giuseppem. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RBViewController : UIViewController
+
+@end
