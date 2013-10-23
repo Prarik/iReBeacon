2eec965785ca9fb25cf487f9fe51700f4c6cad54
Giuseppe Macrì <giuseppem@slalom.com>
2013-10-23 15:43:50 -0700

    Added table view controller to fetch parse data

---
+++ iReBeacon/RBDealListViewController.h
@@ -0,0 +1,13 @@
+//
+//  RBDealListViewController.h
+//  iReBeacon
+//
+//  Created by Giuseppe Macri on 10/23/13.
+//  Copyright (c) 2013 giuseppem. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface RBDealListViewController : PFQueryTableViewController
+
+@end
---
+++ iReBeacon/RBDepartmentListViewController.h
@@ -0,0 +1,13 @@
+//
+//  RBDepartmentsViewController.h
+//  iReBeacon
+//
+//  Created by Giuseppe Macri on 10/23/13.
+//  Copyright (c) 2013 giuseppem. All rights reserved.
+//
+
+#import <Parse/Parse.h>
+
+@interface RBDepartmentListViewController : PFQueryTableViewController
+
+@end
